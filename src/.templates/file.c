23fa4fb6e48f9bc3e759246f746c7bfca72d7a71
Jeremy Musser <jemusser@iu.edu>
2021-10-15 02:06:30 -0400

    Added assignment 04

--- src/.templates/file.c
+++ src/.templates/file.c
@@ -1,3 +1,4 @@
+#include "file.h"
 #include <stdio.h>
 /*
  *  Here is the starting point for your netster part.2 definitions. Add the 
@@ -5,10 +6,10 @@
  */
 
 /* Add function definitions */
-void file_server(char* iface, long port, FILE* fp) {
+void file_server(char* iface, long port, int use_udp, FILE* fp) {
   
 }
 
-void file_client(char* host, long port, FILE* fp) {
+void file_client(char* host, long port, int use_udp, FILE* fp) {
   
 }
--- src/.templates/file.h
+++ src/.templates/file.h
@@ -6,8 +6,19 @@
 #ifndef P2_H
 #define P2_H
 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+
 /* add function prototypes */
-void file_server(char*, long, FILE*);
-void file_client(char*, long, FILE*);
+void file_server(char*, long, int, FILE*);
+void file_client(char*, long, int, FILE*);
+
+/*
+int PROF_build_server_socket(char* iface, long port, int use_udp, struct addrinfo* addr);
+int PROF_build_client_socket(char* host, long port, int use_udp, struct addrinfo* addr);
+*/
+
 
 #endif
